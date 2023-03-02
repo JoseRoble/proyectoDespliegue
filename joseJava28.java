bcf7a87657d07c91e7d5f306e04fbeb59957fec2
root <jose28@jose28>
2023-03-02 11:15:49 +0100

    v1

---
+++ joseJava28.java
@@ -0,0 +1,6 @@
+public class joseJava28 {
+ public static void main(String[] args) {
+ System.out.println("Me llamo Jose y he creado el servidor ubunserver28");
+ System.out.println("Vivo en San Fernando de Henares");
+ }
+}
